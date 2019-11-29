e20fa2f47303bc627a18452dee43cb1da9b9fcb4
kunzalexander <kunz.alexander@hotmail.com>
2019-11-29 19:00:13 +0100

    datenbank klasse erweitert und strukturiert

--- system/db.class.php
+++ system/db.class.php
@@ -16,9 +16,7 @@ class Database{
     } catch (Exception $e) {
       echo "Exception: " . $e->getMessage();
     }
-
   }
-
   private function __connect_db(){
     $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db);
     if($this->conn->connect_error){
@@ -26,35 +24,29 @@ class Database{
     }
   }
 
-  public function select_all_search($table, $field="", $value=""){
+########## START SEARCH FUNCTIONS ##########
+  public function search_all($table, $field="", $valueArr=""){
+    $valueNum = "";
+    if(!empty($valueArr)){
+      $valueNum = count($valueArr);
+    }
     $stmt = $this->conn->stmt_init();
     $sql = $this->__selectFieldTable($table);
-    if(! (empty($field) AND empty($value)) ){
-      $sql .= $this->__whereFieldValue($field);
-      if(!$stmt->prepare($sql)) {
-        echo "error: " . $sql;
-      }
-      else{
-        $stmt->bind_param("s", $value);
-      }
-    }elseif(!$stmt->prepare($sql)){
-      echo "prepare error " . $sql;
-    }
-
+    $sql .= $this->__whereFieldValue($field, $valueNum);
+    $this->__prepareBindParam($stmt, $sql, $valueArr);
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->fetch_assoc();
 
   }
 
-  private function __whereFieldValue($field){
-      $where = " WHERE ";
-      $leftIn = " IN ( ";
-      $rightIn = " )";
-      $param = "?";
-      return $where . $this->__escape($field) . $leftIn . $param . $rightIn;
-  }
+########## END SEARCH FUNCTIONS ##########
 
+########## START INSERT FUNCTIONS ##########
+
+########## END INSERT FUNCTIONS ##########
+
+########## START PREPARED SQL SELECT FUNCTIONS ##########
   private function __selectFieldTable($table, $field=""){
     $select = "SELECT ";
     $from = " FROM ";
@@ -64,8 +56,50 @@ class Database{
       return $select . "*" . $from . $this->__escape($table);
     }
   }
+
+  private function __whereFieldValue($field="", $valueNum=""){
+      $where = " WHERE ";
+      $leftIn = " IN ( ";
+      $rightIn = " )";
+      $param = $this->__paramNum($valueNum);
+      if(!(empty($field) AND empty($valueNum))){
+        return $where . $this->__escape($field) . $leftIn . $param . $rightIn;
+      }
+      else{
+        return;
+      }
+  }
+
+  private function __paramNum($valueNum){
+    $param = "?";
+    if ($valueNum > 1) {
+      for ($i=1; $i<$valueNum ; $i+1) {
+        $param .= "," . $param;
+      }
+    }
+    return $param;
+  }
+
+########## END PREPARED SQL SELECT FUNCTIONS ##########
+
+########## START SECURITY FUNCTIONS ##########
   private function __escape($str){
     return $this->conn->real_escape_string($str);
   }
+
+  private function __prepareBindParam($stmt, $sql, $valueArr=""){
+
+    if(!$stmt->prepare($sql)) {
+      echo "error: " . $sql;
+    }
+    elseif(empty($valueArr)){
+      return;
+    }
+    else{
+      $stmt->bind_param("s", $valueArr);
+    }
+  }
+
+########## START SECURITY FUNCTIONS ##########
 }
 ?>
